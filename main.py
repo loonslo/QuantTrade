6e6b59942bedb23bd8efe214033cb3738fbeb23d
loon alan <loonslo@users.noreply.github.com>
2025-07-12 15:09:20 +0800

    Changes 1

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ TIMEFRAME = '1h'
 LIMIT = 200
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,19 +1,25 @@
 # coding=utf-8
 
-
-
 import ccxt
-print (ccxt.exchanges)
+from config import Config
 
-# # 创建币安交易所对象，verbose=True会输出详细请求日志，便于调试
-# binance = ccxt.binance({
-#     'apiKey': 'yBRGbWLRGxPfxTE4BxQO8LPzZ7GISRgrlhkJdNg95FIqjDf7Is0p3EHP6hZkUFQC',
-#     'secret': 'fnocUgpR443EliLmjV3lTIgU0Sb3UNERprE2GZQJR8kK6gWzDKxWbyAWTt592TPl',
-#     'verbose': True
-# })
-#
+# 创建配置实例
+config = Config(env='development')  # 可选: 'development', 'test', 'production'
+
+# 创建币安交易所对象，使用配置文件
+binance = ccxt.binance(config.get_public_config())
+
+# balance = binance.fetch_balance()
+# print(balance)
+
 # # 加载币安的所有交易对市场信息
 # binance_markets = binance.load_markets()
 #
 # # 打印币安的ID和市场信息
 # print(binance.id, binance_markets)
+#
+symbol = 'BTCUSDT'
+while True:
+    ticker_data = binance.fapipublic_get_ticker_price({'symbol': symbol})
+    print(ticker_data)
+
